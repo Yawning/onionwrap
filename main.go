cfcdea0cbf02db64ad38c0757aaf270d57246bf4
Yawning Angel <yawning@schwanenlied.me>
2015-03-23 17:59:43 +0000

    Add support for rewriting the comand line args.

--- main.go
+++ main.go
@@ -32,6 +32,7 @@ const (
 
 var debugSpew bool
 var quietSpew bool
+var noRewriteArgs bool
 
 func infof(fmt string, args ...interface{}) {
 	if !quietSpew {
@@ -62,40 +63,40 @@ func parsePort(portStr string) (uint16, error) {
 	return uint16(p), nil
 }
 
-func parsePortArg(arg string) (virtPort, target string, err error) {
+func parsePortArg(arg string) (virtPort, targetPort, target string, err error) {
 	// This is formated as VIRTPORT[,TARGET], which is identical to
 	// what the ADD_ONION command expects out of the 'Port' arguments.
 	// If the 'TARGET' is omitted, 'VIRTPORT' is mirrored.  If 'TARGET'
 	// only a naked port, then '127.0.0.1:TARGET' is used, otherwise
 	// 'TARGET' is treated as an address.
 	//
-	// TODO: Look at the rend service code to see how AF_UNIX targets
-	// are handled.
+	// TODO: Figure out what to do with AF_UNIX.
 	if arg == "" {
-		return "", "", errors.New("no Onion Service port specified")
+		return "", "", "", errors.New("no Onion Service port specified")
 	}
 	splitArg := strings.SplitN(arg, ",", 2)
 	virtPort = splitArg[0]
 	if _, err = parsePort(virtPort); err != nil {
-		return "", "", err
+		return "", "", "", err
 	}
 	if len(splitArg) == 1 {
 		// Only a 'VIRTPORT' was provided, mirror it onto the target.
-		return virtPort, localhost + ":" + virtPort, nil
+		return virtPort, virtPort, localhost + ":" + virtPort, nil
 	}
 
 	target = splitArg[1]
 	if _, err = parsePort(target); err == nil {
 		// The 'TARGET' is a naked port.
-		return virtPort, localhost + ":" + target, nil
+		return virtPort, target, localhost + ":" + target, nil
 	}
 	tcpAddr, err := net.ResolveTCPAddr("tcp", target)
 	if err != nil {
-		return "", "", err
+		return "", "", "", err
 	}
 	if tcpAddr.Port == 0 {
-		return "", "", errors.New("target has invalid port '0'")
+		return "", "", "", errors.New("target has invalid port '0'")
 	}
+	targetPort = strconv.Itoa(tcpAddr.Port)
 	return
 }
 
@@ -110,6 +111,7 @@ func main() {
 	hsPortArg := flag.String("port", "", "Onion Service port")
 	flag.BoolVar(&debugSpew, "debug", false, "Print debug messages to stderr")
 	flag.BoolVar(&quietSpew, "quiet", false, "Suppress non-error messages")
+	flag.BoolVar(&noRewriteArgs, "no-rewrite", false, "Disable rewriting subprocess arguments")
 	flag.Parse()
 
 	// The control port is taken from the argument, the env var, and then
@@ -125,14 +127,12 @@ func main() {
 		errorf("Invalid control port: %v\n", err)
 	}
 
-	virtPort, target, err := parsePortArg(*hsPortArg)
+	virtPort, targetPort, target, err := parsePortArg(*hsPortArg)
 	if err != nil {
 		errorf("Invalid virtual port: %v\n", err)
 	}
 
-	// The command that will be fork/execed.  As a convenience for
-	// scripting, %VIRT_PORT in any of the arguments will be replaced
-	// with the virtual port.
+	// The command that will be fork/execed.
 	cmdVec := flag.Args()
 	var cmd *exec.Cmd
 	switch len(cmdVec) {
@@ -146,6 +146,22 @@ func main() {
 	cmd.Stdin = os.Stdin
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
+	if !noRewriteArgs {
+		// Unless explicitly disabled, replace certain variables in the
+		// subprocess command line arguments with values propagated from
+		// the onionwrap command line.
+		//
+		//  * %VPORT - The 'VIRTPORT'.
+		//  * %TPORT - The port component of 'TARGET'.
+		//  * %TADDR - The entire 'TARGET'.
+		for i := 1; i < len(cmd.Args); i++ {
+			v := cmd.Args[i]
+			v = strings.Replace(v, "%VPORT", virtPort, -1)
+			v = strings.Replace(v, "%TPORT", targetPort, -1)
+			v = strings.Replace(v, "%TADDR", target, -1)
+			cmd.Args[i] = v
+		}
+	}
 
 	debugf("Cmd: %v\n", cmd.Args)
 	debugf("CtrlPort: %v, %v\n", ctrlNet, ctrlAddr)
